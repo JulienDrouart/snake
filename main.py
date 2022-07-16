b2be677d08ddab15f778ddf3dae69652fcbbf16b
Julien Drouart <julien.drouart.pro@gmail.com>
2022-07-17 00:07:02 +0200

    Add movement from directions input

--- main.py
+++ main.py
@@ -1,16 +1,62 @@
-# This is a sample Python script.
+import pygame, sys
+import random
+import time
+from pygame.locals import *
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
+screen = pygame.display.set_mode((550, 600))
+clock = pygame.time.Clock()
+pygame.display.set_caption('Snake')
+"""surface = pygame.image.load('logo.jpg')
+pygame.display.set_icon(surface)
+fond = pygame.image.load("fond.jpg")"""
+run = True
+start = False
+snakeHeadCooX= 260
+snakeHeadCooY = 310
+snakeHead = Rect(snakeHeadCooX, snakeHeadCooY, 30, 30)
+direction = "null"
+font = pygame.font.Font('freesansbold.ttf', 32)
+pygame.draw.rect(screen, (255, 0, 0), snakeHead)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+timerEverySeconds = time.time()
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if start is False:
+                timerEverySeconds = time.time()
+                target = pygame.draw.circle(screen, (255, 255, 255),((random.randint(0, 10) * 50 + 25), (random.randint(1, 11)) * 50 + 25), 15)
+                start = True
+            if event.key == pygame.K_UP:
+                direction = "up"
+            if event.key == pygame.K_DOWN:
+                direction = "down"
+            if event.key == pygame.K_LEFT:
+                direction = "left"
+                snakeHeadCooX = snakeHeadCooX-1
+            if event.key == pygame.K_RIGHT:
+                direction = "right"
+                snakeHeadCooX = snakeHeadCooX + 1
+    if time.time() > timerEverySeconds + 1 and start is True:
+        if direction == "down":
+            snakeHeadCooY = snakeHeadCooY + 50
+        if direction == "up":
+            snakeHeadCooY = snakeHeadCooY - 50
+        if direction == "left":
+            snakeHeadCooX = snakeHeadCooX - 50
+        if direction == "right":
+            snakeHeadCooX = snakeHeadCooX + 50
+        timerEverySeconds = time.time()
+        snakeHead = Rect(snakeHeadCooX, snakeHeadCooY, 30, 30)
+        pygame.draw.rect(screen, (255, 0, 0), snakeHead)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pygame.display.flip()
+
+pygame.quit()
+quit()
