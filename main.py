894e98d6d4d61539bb873d4f83cb064dac464c75
Julien Drouart <julien.drouart.pro@gmail.com>
2022-07-24 17:00:12 +0200

    Add eyes and style

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ pygame.draw.rect(screen, (255, 0, 0), snakeHead)
 targetCooX = random.randint(0, 970)
 targetCooY = random.randint(1, 770)
 target = pygame.draw.circle(screen, (255, 255, 255), (targetCooX, targetCooY), 15)
+pygame.draw.circle(screen, (255, 255, 0), (snakeHeadCooX+7, snakeHeadCooY+15), 5)
+pygame.draw.circle(screen, (255, 255, 0), (snakeHeadCooX+22, snakeHeadCooY+15), 5)
 score = 0
 snakeBody = []
 clock = pygame.time.Clock()
@@ -41,7 +43,6 @@ while run:
             if event.key == pygame.K_RIGHT:
                 direction = "right"
 
-
     if start is True:
         if direction == "down":
             snakeHeadCooY = snakeHeadCooY + snake_speed
@@ -56,33 +57,30 @@ while run:
         if snakeHeadCooX < 0 or snakeHeadCooX > 970 or snakeHeadCooY < 0 or snakeHeadCooY > 770:
             run = False
 
-
         snakeBody.append([snakeHeadCooX, snakeHeadCooY])
         if len(snakeBody) > snake_length:
             del snakeBody[0]
 
-
         """ if the snake catch the target"""
         if pygame.Rect.colliderect(target, snakeHead):
-
             score += 1
             targetCooX = (random.randint(0, 10) * 50) + 25
             targetCooY = (random.randint(1, 11) * 50) + 25
             snake_speed += 1
             snake_length += 1
 
-
-
-
         screen.fill((0, 0, 0))
         target = pygame.draw.circle(screen, (255, 255, 255), (targetCooX, targetCooY), 15)
         snakeHead = Rect(snakeHeadCooX, snakeHeadCooY, 30, 30)
         pygame.draw.rect(screen, (255, 0, 0), snakeHead)
+
+
         font = pygame.font.SysFont(None, 24)
         img = font.render('Score : ' + str(score), True, (255, 255, 255))
         for element in snakeBody:
-            pygame.draw.rect(screen, (255, 255, 255), [element[0], element[1], 30, 30])
-
+            pygame.draw.rect(screen, (255, 0, 0), [element[0], element[1], 30, 30])
+        pygame.draw.circle(screen, (255, 255, 0), (snakeHeadCooX + 7, snakeHeadCooY + 15), 5)
+        pygame.draw.circle(screen, (255, 255, 0), (snakeHeadCooX + 22, snakeHeadCooY + 15), 5)
         screen.blit(img, (20, 20))
     clock.tick(30)
     pygame.display.flip()
